af13ed16de6417336f45c6fcf767f50c0e9caa7e
HissDev <hissoffc@gmail.com>
2024-06-19 23:37:16 +0300

    Добавил фильтры для поиска пользователей по id

--- src/views/AdminUserView.vue
+++ src/views/AdminUserView.vue
@@ -1,4 +1,36 @@
 <template>
+  <div class="filters">
+    <div class="search-container">
+      <input
+        type="text"
+        placeholder="Поиск"
+        class="search-input"
+        v-model="getInputSearch"
+        @keyup="getProductsBySearch"
+      />
+      <svg
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        class="search-icon"
+      >
+        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+        <g
+          id="SVGRepo_tracerCarrier"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        ></g>
+        <g id="SVGRepo_iconCarrier">
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M15 10.5C15 12.9853 12.9853 15 10.5 15C8.01472 15 6 12.9853 6 10.5C6 8.01472 8.01472 6 10.5 6C12.9853 6 15 8.01472 15 10.5ZM14.1793 15.2399C13.1632 16.0297 11.8865 16.5 10.5 16.5C7.18629 16.5 4.5 13.8137 4.5 10.5C4.5 7.18629 7.18629 4.5 10.5 4.5C13.8137 4.5 16.5 7.18629 16.5 10.5C16.5 11.8865 16.0297 13.1632 15.2399 14.1792L20.0304 18.9697L18.9697 20.0303L14.1793 15.2399Z"
+            fill="#6583a2"
+          ></path>
+        </g>
+      </svg>
+    </div>
+  </div>
   <div class="table-container">
     <table class="table">
       <thead>
@@ -57,6 +89,7 @@ export default {
       users: [],
       userId: null,
       userRoles: ["ADMIN", "USER"],
+      getInputSearch: null
     };
   },
 
@@ -106,6 +139,30 @@ export default {
         .patch("http://localhost:8080/users/" + id, { role: role }, { headers })
         .then((response) => {
           // console.log(response);
+        })
+        .catch((error) => console.log(error));
+    },
+
+    getProductsBySearch() {
+      const token = localStorage.getItem("token");
+      const headers = {
+        Authorization: `Bearer ${token}`,
+      };
+      if (!this.getInputSearch) {
+        this.getUsers()
+      }
+      axios
+        .get(
+          `http://localhost:8080/user/${this.getInputSearch}`, {headers}
+        )
+        .then((response) => {
+          const data = response.data;
+          console.log(data);
+          this.users = data
+          // this.users = data.content;
+          // this.totalElements = data.totalElements;
+          // this.url = "user/" + this.getInputSearch;
+          // this.previewLoading = false;
         })
         .catch((error) => console.log(error));
     },
@@ -177,5 +234,30 @@ export default {
   .table-container {
     overflow-x: auto;
   }
+}
+
+/* Фильтры */
+.filters {
+  display: flex;
+  justify-content: space-between;
+}
+
+input {
+  width: auto;
+}
+
+.search-container {
+  position: relative;
+  display: inline-block;
+}
+
+.search-icon {
+  position: absolute;
+  top: 43%;
+  right: 8px;
+  transform: translateY(-50%);
+  pointer-events: none;
+  color: #888;
+  width: 40px;
 }
 </style>
